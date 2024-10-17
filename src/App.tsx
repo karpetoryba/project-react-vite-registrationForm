19b999894b0769cc3b6354fd0d8fe9721683eb7d
karpetoryba <karpenkokarpenko24@gmail.com>
2024-10-17 15:31:11 +0200

    fix step 4 text

--- src/App.tsx
+++ src/App.tsx
@@ -161,14 +161,18 @@ function SimpleForm() {
         );
       case 4:
         return (
-          <div className="flex flex-col items-center p-6">
+          <div className="flex flex-col items-center p-6 text-center">
             <div className="bg-green-500 text-white rounded-full text-6xl p-8 mb-4 w-24 h-24 flex items-center justify-center">
               <span className="text-white text-5xl">✓</span>
             </div>
 
-            <p className="text-2xl font-bold">Congrats {formData.name}</p>
-            <p className="text-xl font-bold">Registration Successful</p>
-            <p className="text-lg font-bold">
+            <p className="text-2xl font-bold break-words">
+              Congrats {formData.name}
+            </p>
+            <p className="text-xl font-bold break-words">
+              Registration Successful
+            </p>
+            <p className="text-lg font-bold break-words">
               Email confirmation sent to {formData.email}
             </p>
           </div>
